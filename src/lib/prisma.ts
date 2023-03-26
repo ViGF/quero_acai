642eb13f471fc38c102f2f60be8f0855e3c20685
ViGF <vitoriogdef@gmail.com>
2023-03-26 10:16:48 -0300

    fix: prisma client init

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,17 @@
-// lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+    let globalWithPrisma = global as typeof globalThis & {
+        prisma: PrismaClient;
+    };
+    if (!globalWithPrisma.prisma) {
+        globalWithPrisma.prisma = new PrismaClient();
+    }
+    prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
